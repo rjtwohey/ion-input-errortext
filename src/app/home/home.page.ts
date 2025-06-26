03834fa8bf7a3942ff5e47461480e054d653f343
Ryan Twohey <rtwohey@ionic.io>
2025-06-26 15:04:38 -0400

    fixed errorText appearing

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,55 +1,18 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon , IonInput, IonItem, IonList} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonInput, IonItem, IonList],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonInput, IonItem, IonList, FormsModule],
 })
 export class HomePage {
-  loginForm: FormGroup;
-  usernameLabel = 'Username';
-  usernameNote = 'Enter your username';
-  usernameError = '';
-  isRegularInvalid = false;
-
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(4)]],
-    });
-
-    this.usernameControl.statusChanges.subscribe(() => {
-      this.setUsernameError();
-    });
+  email: string = '';
+  // ...existing code...
+  constructor() {
+   
   }
 
-  get usernameControl(): AbstractControl {
-    return this.loginForm.get('username')!;
-  }
-
-  private setUsernameError() {
-    const control = this.usernameControl;
-    if (control.touched && control.invalid) {
-      if (control.errors?.['required']) {
-        this.usernameError = 'Username is required';
-      } else if (control.errors?.['minlength']) {
-        this.usernameError = 'Username must be at least 3 characters';
-      } else {
-        this.usernameError = 'Invalid username';
-      }
-    } else {
-      this.usernameError = '';
-    }
-  }
-
-  validateRegularEmail(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    this.isRegularInvalid = !emailRegex.test(input.value);
-
-    // Dynamically update `aria-invalid` attribute for <input>
-    input.setAttribute('aria-invalid', this.isRegularInvalid.toString());
-  }
 }
